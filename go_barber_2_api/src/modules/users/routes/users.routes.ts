19a9d131041effad7cf66fd6e26ddad72b0eff45
divyjeet <152507238+sento-12@users.noreply.github.com>
2025-07-06 17:26:34 +0530

    Update users.routes.ts "" number

--- go_barber_2_api/src/modules/users/routes/users.routes.ts
+++ go_barber_2_api/src/modules/users/routes/users.routes.ts
@@ -12,7 +12,7 @@ usersRouter.post(
   celebrate({
     [Segments.BODY]: {
       name: Joi.string().required(),
-      email: Joi.string().email().required(),
+      email: Joi.string().required(),
       password: Joi.string().required(),
     },
   }),
